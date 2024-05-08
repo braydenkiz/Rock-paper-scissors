ef655631d6ea892b8b43940f06aa6592199a507d
brayden kiz <115608437+braydenkiz@users.noreply.github.com>
2024-05-08 18:14:18 -0500

    got getComputerChoice to work

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    let cpu = Math.ceil(Math.random() * 3);
+    if(cpu == 1){
+        return "rock";
+    }else if(cpu == 2){
+        return "paper";
+    }else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
